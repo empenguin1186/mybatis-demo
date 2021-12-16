8388b30843f07f087893b7f9c2e6cba309f8ace5
empenguin1186 <emperorpenguint1186@gmail.com>
2021-12-16 23:23:59 +0900

    :bug: convert camel case to snake case

--- src/test/kotlin/jp/co/penguin/mybatisdemo/infra/repository/mapper/BookWithRentalMapperTest.kt
+++ src/test/kotlin/jp/co/penguin/mybatisdemo/infra/repository/mapper/BookWithRentalMapperTest.kt
@@ -47,7 +47,7 @@ internal class BookWithRentalMapperTest {
             registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl)
             registry.add("spring.datasource.username", mysqlContainer::getUsername)
             registry.add("spring.datasource.password", mysqlContainer::getPassword)
-            registry.add("spring.jackson.property-naming-strategy") { "SNAKE_CASE" }
+            registry.add("mybatis.configuration.map-underscore-to-camel-case") { true }
         }
     }
 
